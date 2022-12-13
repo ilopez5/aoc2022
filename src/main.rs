6a58545ba677f6c8fecd721ec52ad95b574f91ee
Ismael J Lopez <ismaelopezandrade@gmail.com>
2022-12-12 23:14:56 -0600

    day 9

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,166 @@
+#[derive(Copy, Clone, Debug)]
+struct Pos {
+    x: i32,
+    y: i32,
+}
+
+impl Pos {
+    pub fn new(x: i32, y: i32) -> Self {
+        Self { x: x, y: y }
+    }
+
+    pub fn clone(&self) -> Self {
+        Self {
+            x: self.x,
+            y: self.y,
+        }
+    }
+
+    pub fn move_right(&mut self, steps: i32) {
+        self.y += steps;
+    }
+    pub fn move_left(&mut self, steps: i32) {
+        self.y -= steps;
+    }
+    pub fn move_up(&mut self, steps: i32) {
+        self.x -= steps;
+    }
+    pub fn move_down(&mut self, steps: i32) {
+        self.x += steps;
+    }
+
+    pub fn lead(&mut self, direction: char, distance: i32) -> Vec<Self> {
+        let mut moves = Vec::<Self>::new();
+        match direction {
+            'U' => {
+                for _ in 0..distance {
+                    self.move_up(1);
+                    moves.push(self.clone());
+                }
+            }
+            'D' => {
+                for _ in 0..distance {
+                    self.move_down(1);
+                    moves.push(self.clone());
+                }
+            }
+            'L' => {
+                for _ in 0..distance {
+                    self.move_left(1);
+                    moves.push(self.clone());
+                }
+            }
+            'R' => {
+                for _ in 0..distance {
+                    self.move_right(1);
+                    moves.push(self.clone());
+                }
+            }
+            _ => (),
+        };
+        moves
+    }
+
+    pub fn follow(&mut self, head: &Self) -> Vec<Self> {
+        let mut moves = Vec::<Self>::new();
+        while !self.hugs(&head) {
+            //move vertically
+            if self.x > head.x {
+                self.move_up(1);
+            } else if self.x < head.x {
+                self.move_down(1);
+            }
+            //move horizontally
+            if self.y > head.y {
+                self.move_left(1);
+            } else if self.y < head.y {
+                self.move_right(1);
+            }
+            moves.push(self.clone());
+        }
+        moves
+    }
+
+    fn hugs(&self, other: &Self) -> bool {
+        (self.x - other.x).abs() <= 1 && (self.y - other.y).abs() <= 1
+    }
+}
+
 fn main() {
-    let input = include_str!("inputs/input8.txt");
+    let input = include_str!("inputs/input9.txt");
+    let lines: Vec<&str> = input.lines().collect();
 
-    let grid: Vec<Vec<u32>> = input
-        .lines()
-        .map(|line| line.chars()
-             .map(|c| c.to_digit(10).unwrap())
-             .collect())
-        .collect();
+    let n = 600;
+    let mut grid: Vec<Vec<u32>> = vec![vec![0; n]; n];
+    grid[n / 2 - 1][n / 2 - 1] = 1;
 
-    let part1 = part1(&grid);
+    let part1 = solution(&mut grid.clone(), &lines, 2);
     println!("Part 1: {}", part1);
-    
-    let part2 = part2(&grid);
+
+    let part2 = solution(&mut grid.clone(), &lines, 10);
     println!("Part 2: {}", part2);
 }
 
-fn part1(grid: &[Vec<u32>]) -> u32 {
-    let dim = grid.len() - 1;
-    let mut visible = 0;
+fn _print_grid(grid: &[Vec<u32>]) {
+    for row in grid {
+        let r: String = row.iter().map(|c| if c > &0 { '#' } else { '.' }).collect();
+        println!("{r}");
+    }
+}
+
+fn _print_rope(grid: &[Vec<u32>], rope: &[Pos]) {
     for (r, row) in grid.iter().enumerate() {
-        for (c, tree) in row.iter().enumerate() {
-            visible += if r == 0 || c == 0 || r == dim || c == dim {
-                1
-            } else if tree == &0 {
-                0
-            } else if tree > row[..c].iter().max().unwrap() {
-                1
-            } else if tree > row[c+1..].iter().max().unwrap() {
-                1
-            } else if tree > grid[..r].iter().map(|rw| &rw[c]).max().unwrap() {
-                1
-            } else if tree > grid[r+1..].iter().map(|rw| &rw[c]).max().unwrap() {
-                1
+        let mut show = Vec::<char>::new();
+        for c in 0..row.len() {
+            let mut ch: char = '.';
+            for (i, knot) in rope.iter().enumerate() {
+                if knot.x == r.try_into().unwrap() && knot.y == c.try_into().unwrap() {
+                    if i == 0 {
+                        ch = 'H';
+                    } else {
+                        ch = char::from_digit(i as u32, 10).unwrap();
+                    }
+                    break;
+                }
+            }
+            show.push(ch);
+        }
+        println!("{}", show.iter().collect::<String>())
+    }
+}
+
+fn solution(grid: &mut Vec<Vec<u32>>, lines: &[&str], rope_len: usize) -> u32 {
+    let dim = (grid.len() as i32) / 2 - 1;
+    let mut rope: Vec<Pos> = vec![Pos::new(dim, dim); rope_len];
+
+    for line in lines {
+        let instr: Vec<&str> = line.split_whitespace().collect();
+        let direction: char = instr[0].chars().next().unwrap();
+        let distance: i32 = instr[1].parse().unwrap();
+
+        let mut moves: Vec<Pos> = vec![];
+        let mut new_moves: Vec<Pos> = vec![];
+
+        for i in 0..rope.len() {
+            let knot = &mut rope[i];
+            if i == 0 {
+                new_moves.extend(knot.lead(direction, distance));
             } else {
-                0
-            };
-        }
-    }
-    visible
-}
-
-
-fn part2(grid: &Vec<Vec<u32>>) -> u32 {
-    let dim = grid.len() - 1;
-    let mut max_scenic_score = 0;
-
-    for (r, row) in grid.iter().enumerate() {
-        for (c, tree) in row.iter().enumerate() {
-            if r == 0 || c == 0 || r == dim || c == dim {
-                continue;
+                for m in moves {
+                    new_moves.append(&mut knot.follow(&m));
+                }
             }
+            moves = new_moves.clone();
+            new_moves.clear();
+        }
 
-            let left = {
-                let range = row[..c].iter().rev();
-                get_scenic_score(range, *tree)
-            };
-
-            let right = get_scenic_score(&row[c+1..], *tree);
-
-            let above = {
-                let range = grid[..r].iter().map(|_row| &_row[c]).rev();
-                get_scenic_score(range, *tree)
-            };
-
-            let below = {
-                let range = grid[r+1..].iter().map(|_row| &_row[c]);
-                get_scenic_score(range, *tree)
-            };
-            
-            let score = left * right * above * below;
-            if score > max_scenic_score {
-                max_scenic_score = score;
-            }
+        // update the grid with last knot's moves (tail)
+        for m in &moves {
+            let row = m.x as usize;
+            let col = m.y as usize;
+            grid[row][col] = 1;
         }
     }
-    max_scenic_score
-}
 
-fn get_scenic_score<'a>(los: impl IntoIterator<Item = &'a u32>, tree: u32) -> u32 {
-    let mut score: u32 = 0;
-    for neighbor in los {
-        score += 1;
-        if neighbor >= &tree {
-            break;
-        }
-    }
-    score
+    grid.iter().map(|l| l.iter().sum::<u32>()).sum()
 }
